095d2e67b11b3cc52784141b57bb90a44236cbb6
Renan Frutuozo <frutuozo29@gmail.com>
2017-11-13 13:58:16 -0300

    Configurando o servidor, modulos e middlewares

---
+++ config/server.js
@@ -0,0 +1,31 @@
+/** Importação do modulo express */
+var express = require('express');
+
+/** Importação do modulo consign */
+var consign = require('consign');
+
+/** Importação do modulo body-parser */
+var bodyParser = require('body-parser');
+
+/** Importação do modulo express-validator */
+var expressValidator = require('express-validator');
+
+/** Inicialização do express */
+var app = express();
+
+/** Configurações de modulos e middlewares do express */
+app.set('view engine', 'ejs');
+app.set('view', './app/views');
+
+app.use(express.static('./app/public'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+consign()
+    .include('app/routes')
+    .then('app/models')
+    .then('app/controllers')
+    .into(app);
+
+/** Exportação do objeto APP */
+module.exports = app;
